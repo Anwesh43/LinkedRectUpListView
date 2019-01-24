9d7eb5e7b611823fe68000a68767e5798664468d
Anwesh43 <anweshthecool0@gmail.com>
2019-01-24 09:42:53 +0530

    adding libraries and defining constants

--- rectuplistview/src/main/java/com/anwesh/uiprojects/rectuplistview/RectUpListView.kt
+++ rectuplistview/src/main/java/com/anwesh/uiprojects/rectuplistview/RectUpListView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.rectuplistview
 /**
  * Created by anweshmishra on 24/01/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val nodes : Int = 5
+val rects : Int = 5
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.8f
+val foreColor : Int = Color.parseColor("#1565C0")
