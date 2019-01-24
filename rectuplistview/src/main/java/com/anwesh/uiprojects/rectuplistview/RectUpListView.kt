23e19fb0467e14d9747edcd1ec293b92b6abb3bd
Anwesh43 <anweshthecool0@gmail.com>
2019-01-24 09:50:12 +0530

    adding Int and Float extension methods

--- rectuplistview/src/main/java/com/anwesh/uiprojects/rectuplistview/RectUpListView.kt
+++ rectuplistview/src/main/java/com/anwesh/uiprojects/rectuplistview/RectUpListView.kt
@@ -21,6 +21,13 @@ val strokeFactor : Int = 90
 val sizeFactor : Float = 2.8f
 val foreColor : Int = Color.parseColor("#1565C0")
 
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
+fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
+fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
 class RectUpListView(ctx : Context) : View(ctx) {
 
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
