985957f92e7b5c5356d7e00323800ee5814c822e
Anwesh43 <anweshthecool0@gmail.com>
2019-01-24 09:59:05 +0530

    adding drawRULNode canvas extension method

--- rectuplistview/src/main/java/com/anwesh/uiprojects/rectuplistview/RectUpListView.kt
+++ rectuplistview/src/main/java/com/anwesh/uiprojects/rectuplistview/RectUpListView.kt
@@ -17,8 +17,6 @@ val nodes : Int = 5
 val rects : Int = 5
 val scGap : Float = 0.05f
 val scDiv : Double = 0.51
-val strokeFactor : Int = 90
-val sizeFactor : Float = 2.8f
 val foreColor : Int = Color.parseColor("#1565C0")
 
 fun Int.inverse() : Float = 1f / this
@@ -27,6 +25,29 @@ fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale
 fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
 fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
+fun Canvas.drawRULNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val rSize : Float = gap / (rects + 1)
+    paint.color = foreColor
+    val x : Float = (rSize * rects) / 2
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    save()
+    translate(w/2 - x, i * (gap + 1) - rSize)
+    for (j in 0..(rects - 1)) {
+        val scj1 : Float = sc1.divideScale(j, rects)
+        val scj2 : Float = sc2.divideScale(j, rects)
+        val size : Float = (rSize/2) * scj1
+        save()
+        translate(rSize * j,  -rSize / 2 - (rSize * (j + 1)) * scj2)
+        drawRect(RectF(-size, -size, size, size), paint)
+        restore()
+    }
+    restore()
+}
 
 class RectUpListView(ctx : Context) : View(ctx) {
 
